06060faea998ab133111aa5eee6ba57e4e4372c2
Shreyansh Prakash <shreyanshprakash@gentlemachine-2.local>
2025-03-22 23:14:26 +0530

    [feature/machine-code-problems] : Added logic to start and reset time plus stop timer as well

--- src/MachineCodeProblems/StopWatch/StopWatch.tsx
+++ src/MachineCodeProblems/StopWatch/StopWatch.tsx
@@ -26,12 +26,14 @@ class TimeStateModel {
   min: number;
   sec: number;
   milliSeconds: number;
+  template: string;
   constructor() {
     this.isRunning = false;
     this.hour = 0;
     this.min = 0;
     this.sec = 0;
     this.milliSeconds = 0;
+    this.template = "";
   }
 }
 
@@ -39,19 +41,66 @@ class TimeStateModel {
     ====================================================== [Util Methods]======================================================
 */
 
-const handleTimeProcessing = () => {
+const handleTimeProcessing = (startTime: number) => {
   const date = new Date();
 
-  const hour = date.getHours();
-  const min = date.getMinutes();
-  const sec = date.getSeconds();
-  const milliSeconds = date.getMilliseconds();
+  const currentTime = date.getTime();
+  const timeElapsed = currentTime - startTime;
+
+  const diffTime = 5 * 60 * 60 * 1000 + 30 * 60 * 1000; // 5 hrs, 30 mins, 0 secs 1970
+  const newDate = new Date(timeElapsed - diffTime);
+
+  const hour = newDate.getHours();
+  const min = newDate.getMinutes();
+  const sec = newDate.getSeconds();
+  const milliSeconds = newDate.getMilliseconds();
+
+  const padHour = String(hour).padStart(2, "00");
+  const padMin = String(min).padStart(2, "00");
+  const padSec = String(sec).padStart(2, "00");
+  const padMillSeconds = String(milliSeconds).padStart(2, "00");
+
+  const template = `${padHour} : ${padMin} : ${padSec}`;
 
   return {
     hour,
     min,
     sec,
     milliSeconds,
+    template,
+  };
+};
+
+const handleTimeProcessingUsingTimeDiff = (startTime: number) => {
+  let noOfDays = 0;
+  const date = new Date();
+
+  const currentTime = date.getTime() - noOfDays * (24 * 60 * 60 * 1000);
+  const timeElapsed = currentTime - startTime;
+
+  const hour = Math.floor(timeElapsed / (60 * 60 * 1000));
+  const min = Math.floor(timeElapsed / (60 * 1000) % 60);
+  const sec = Math.floor(timeElapsed / 1000 % 60);
+  const milliSeconds = Math.floor(timeElapsed / 1);
+
+  const padHour = String(hour).padStart(2, "00");
+  const padMin = String(min).padStart(2, "00");
+  const padSec = String(sec).padStart(2, "00");
+  const padMillSeconds = String(milliSeconds).padStart(2, "00");
+
+  const template = `${padHour} : ${padMin} : ${padSec}`;
+
+  const isHourEquals24 = Math.floor(hour / 24) >= 0;
+  if (isHourEquals24) {
+    noOfDays++;
+  }
+
+  return {
+    hour,
+    min,
+    sec,
+    milliSeconds,
+    template,
   };
 };
 
@@ -63,15 +112,16 @@ export const StopWatch = () => {
   const [timeState, setTimeState] = useState<TimeStateModel>(
     new TimeStateModel()
   );
-  const setIntervalInst = useRef<any>(null);
+  const setIntervalInstRef = useRef<any>(null);
+  const timerStartTimeRef = useRef<number>(0);
 
   useEffect(() => {
-    return () => clearInterval(setIntervalInst.current);
+    return () => clearInterval(setIntervalInstRef.current);
   }, []);
 
   const startStopWatch = (): ReturnType<typeof setInterval> => {
     const intervalInst = setInterval(() => {
-      const result = handleTimeProcessing();
+      const result = handleTimeProcessingUsingTimeDiff(timerStartTimeRef.current);
       setTimeState((prev) => ({
         ...prev,
         ...result,
@@ -81,28 +131,40 @@ export const StopWatch = () => {
   };
 
   const handleStartTimer = () => {
-    const result = handleTimeProcessing();
+    timerStartTimeRef.current = new Date().getTime();
+    const result = handleTimeProcessingUsingTimeDiff(timerStartTimeRef.current);
     setTimeState((prev) => ({ ...prev, isRunning: true, ...result }));
     const intervalInst = startStopWatch();
-    setIntervalInst.current = intervalInst;
+    setIntervalInstRef.current = intervalInst;
+  };
+
+  const handleStopTimer = () => {
+    clearInterval(setIntervalInstRef.current);
+    setIntervalInstRef.current = null;
   };
 
   const handleResetTimer = () => {
-    clearInterval(setIntervalInst.current);
-    setIntervalInst.current = null;
+    clearInterval(setIntervalInstRef.current);
+    setIntervalInstRef.current = null;
     setTimeState(new TimeStateModel());
   };
 
   return (
     <StopWatchStyles>
-      <div className="time-wrapper">
+      {/* <div className="time-wrapper">
         <ElementStyle>{timeState?.hour}</ElementStyle>
         <ElementStyle>{timeState?.min}</ElementStyle>
         <ElementStyle>{timeState?.sec}</ElementStyle>
-      </div>
+      </div> */}
+      <ElementStyle className="time-wrapper-pad">
+        {timeState.template}
+      </ElementStyle>
       <div className="action-buttons-wrapper">
         <button onClick={handleStartTimer} disabled={timeState.isRunning}>
           Start Timer
+        </button>
+        <button onClick={handleStopTimer} disabled={!timeState.isRunning}>
+          Stop Timer
         </button>
         <button onClick={handleResetTimer} disabled={!timeState.isRunning}>
           Reset Timer
